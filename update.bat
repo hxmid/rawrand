9b79aded61c565de33aa06f5521b3e2926bd3db5
hamid <hamid.haxhimolla@gmail.com>
2024-12-02 04:20:20 +1100

    updated update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,6 @@
-@echo off
+winget install --id=Chromium.ChromeDriver -e --accept-package-agreements --accept-source-agreements
 copy /y ..\settings.json .
 git fetch
 git pull
 python -m pip install -r requirements.txt
+echo "done :)"
